355f6b3f78d513846ae0f9ee46caaf63f97da6ce
Andrew Fenton <andrewfenton898@gmail.com>
2025-04-18 18:50:57 -0400

    fix: config and front matter

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,6 @@ import vercel from '@astrojs/vercel/serverless'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://andrew-fenton.github.io',
-	base: 'personal-site',
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		tailwind({
